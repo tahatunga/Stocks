91dc9076a4cca89bd24635f077dd78d21267af20
Tisho Ganev <tisho@bgdeveloper.com>
2025-11-30 11:18:14 +0200

    Views structure

---
+++ Stocks/StockDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  StockDetailsView.swift
+//  Stocks
+//
+//  Created by Tihomir Ganev on 30.11.25.
+//
+
+import SwiftUI
+
+struct StockDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockDetailsView()
+}
---
+++ Stocks/StockRowView.swift
@@ -0,0 +1,18 @@
+//
+//  StockRowView.swift
+//  Stocks
+//
+//  Created by Tihomir Ganev on 30.11.25.
+//
+
+import SwiftUI
+
+struct StockRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StockRowView()
+}

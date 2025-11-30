798dafd0ad193536fbc70ba0e2e41211247e5c3f
Tisho Ganev <tisho@bgdeveloper.com>
2025-11-30 11:35:27 +0200

    Create StockRowModel.swift

---
+++ Stocks/Stocks/StockRowModel.swift
@@ -0,0 +1,15 @@
+//
+//  StockRowModel.swift
+//  Stocks
+//
+//  Created by Tihomir Ganev on 30.11.25.
+//
+
+import Foundation
+
+struct StockRowModel {
+    
+    let stockName: String
+    var stockPrice: Double
+    var previosPrice: Double?
+}

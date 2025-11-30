f5f1c83ada2d3ae2b69dbf7c168d74bdec6e526c
Tisho Ganev <tisho@bgdeveloper.com>
2025-11-30 11:01:00 +0200

    Initial commit

---
+++ Stocks/Stocks/StocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksApp.swift
+//  Stocks
+//
+//  Created by Tihomir Ganev on 30.11.25.
+//
+
+import SwiftUI
+
+@main
+struct StocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
